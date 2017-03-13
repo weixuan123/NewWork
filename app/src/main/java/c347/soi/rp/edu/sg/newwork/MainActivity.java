937e6e5d78a93d30de5b404b8128a67070393d0c
NG WEI XUAN <14056864@rp.edu.sg>
2017-03-13 15:48:13 +0800

    Initial commit

---
+++ app/src/main/java/c347/soi/rp/edu/sg/newwork/MainActivity.java
@@ -0,0 +1,13 @@
+package c347.soi.rp.edu.sg.newwork;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
