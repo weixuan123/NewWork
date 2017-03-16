dcda75d120c4ad40eb9b149b2d203f36dc5eac51
14056864 <14056864@rp.edu.sg>
2017-03-17 01:16:34 +0800

    MainActivity.java add button textview and some other stuff based on the tutorial and now seeing it like just database still learning i guess

--- app/src/main/java/c347/soi/rp/edu/sg/newwork/MainActivity.java
+++ app/src/main/java/c347/soi/rp/edu/sg/newwork/MainActivity.java
@@ -2,6 +2,12 @@ package c347.soi.rp.edu.sg.newwork;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import io.realm.Realm;
 import io.realm.RealmObject;
@@ -10,15 +16,36 @@ import io.realm.RealmResults;
 public class MainActivity extends AppCompatActivity {
 
     private Realm realm;
+    private EditText editText1;
+    private EditText editText2;
+    private TextView tv;
+    private Button btn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        editText1 = (EditText) findViewById(R.id.editText1);
+        editText2 = (EditText) findViewById(R.id.editText2);
+        tv = (TextView) findViewById(R.id.textView);
+        btn = (Button) findViewById(R.id.button);
+
+
+
+
         // Create relam instance
         Realm.init(this);
         realm = Realm.getDefaultInstance();
+
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                save_into_database(editText1.getText().toString().trim(), Integer.parseInt(editText2.getText().toString().trim()));
+                refresh_views();
+            }
+        });
 
         realm.executeTransaction(new Realm.Transaction() {
             @Override
@@ -34,5 +61,37 @@ public class MainActivity extends AppCompatActivity {
         RealmResults<Person> personRealmObject = realm.where(Person.class).findAll();
         // See the outcome
         System.out.println(personRealmObject);
+    }
+
+    private void refresh_views() {
+        //find out the things in database and can set the conditions
+        RealmResults<Person> result2 = realm.where(Person.class)
+                .equalTo("name", "John")
+                .or()
+                .equalTo("name", "Peter")
+                .findAll();
+    }
+
+    private void save_into_database(final String name, final int age){
+        realm.executeTransactionAsync(new Realm.Transaction() {
+            @Override
+            public void execute(Realm bgRealm) {
+                Person person = bgRealm.createObject(Person.class);
+                person.setName(name);
+                person.setAge(age);
+            }
+        }, new Realm.Transaction.OnSuccess() {
+            @Override
+            public void onSuccess() {
+                tv.v("Database added");
+            }
+        }, new Realm.Transaction.OnError() {
+            @Override
+            public void onError(Throwable error) {
+                // Transaction failed and was automatically canceled.
+                tv.v("Database  not added");
+            }
+        });
+
     }
 }
